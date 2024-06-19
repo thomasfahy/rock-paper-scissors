f5e6a2a1cb94ef0c2363d6dca6522ec3b84c68ba
thomasfahy <thomasfahy11@gmail.com>
2024-06-19 18:04:47 +0100

    feat: add computer choice logic

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getComputerChoice(){
     choice = Math.random();
     console.log(choice);
+    if (choice <= 0.3333){
+        action = "rock";
+    }
+    else if (choice <= 0.6666){
+        action = "scissors";
+    }
+    else{
+        action = "paper";
+    }
+    console.log(action);
+    return action;
+
 }
+getComputerChoice()
