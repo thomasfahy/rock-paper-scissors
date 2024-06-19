453b34b793f7f8e6501d08533c868ce817bbc33b
thomasfahy <thomasfahy11@gmail.com>
2024-06-19 18:20:37 +0100

    feat: add game logic in javascript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getComputerChoice(){
-    choice = Math.random();
+    var choice = Math.random();
     console.log(choice);
     if (choice <= 0.3333){
         action = "rock";
@@ -15,3 +18,21 @@ function getComputerChoice(){
 
 }
 getComputerChoice()
+
+function getHumanChoice(){
+    var userChoice = prompt("Please choose: 'rock' 'paper' or 'scissors'");
+    return userChoice;
+}
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        alert("Its a tie!");
+    }
+    else if((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock")) {
+        alert(humanChoice + " vs " + computerChoice + ". You win!")
+    }
+    else if((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "paper")) {
+        alert(humanChoice + " vs " + computerChoice + ". You win!")
+    }
+}
+
+playRound(getHumanChoice(),getComputerChoice());
